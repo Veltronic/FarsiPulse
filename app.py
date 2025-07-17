51e0a866bf31b196327a86504ea83a86fbc02d13
Veltronic <veltronic.official@gmail.com>
2025-07-17 02:46:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ CORS(app)
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-MODEL_DIR = r""
+MODEL_DIR = r"" # model Address
 
 tokenizer = None
 model = None
